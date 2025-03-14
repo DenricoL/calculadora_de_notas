9b03a32ed39eff75dba55ba2dbab6925d607b588
Douglas <163703664+DenricoL@users.noreply.github.com>
2025-03-14 11:11:38 -0300

    Add files via upload

--- calculadora.js
+++ calculadora.js
@@ -2,6 +2,7 @@ function calcularNota() {
     let N1 = parseFloat(document.getElementById("n1").value);
     let N2 = parseFloat(document.getElementById("n2").value);
     let resultado = document.getElementById("resultado");
+    let container = document.querySelector(".container");
 
     if (isNaN(N1) || isNaN(N2)) {
         resultado.innerHTML = "Por favor, insira valores válidos.";
@@ -10,17 +11,43 @@ function calcularNota() {
     }
 
     let N3 = (12 - N1 * 0.3) / 0.85 - N2;
-    resultado.innerHTML = `Nota necessária para média 6: <strong>${N3.toFixed(1)}</strong>`;
-    if (N3 <= 5)
-    resultado.style.color = "green";
-    else if (N3 > 5 && N3 < 10)
-    resultado.style.color = "#8a2101"
+    if (N3 > 10) 
+    {
+        let N3 = (10 - N1 * 0.3) / 0.85 - N2;
+        if (N3 > 10)
+        {
+            resultado.innerHTML = `Reprovado.`;
+            container.classList.remove("good");
+            container.classList.add("bad");
+            resultado.style.color = "red";
+        }
+        else
+            resultado.innerHTML = `Nota necessária pra ir pra IFA: <strong>${N3.toFixed(2)}</strong>`;
+            resultado.style.color = "red";
+            container.classList.remove("good");
+            container.classList.add("bad");
+    }    
     else
-    resultado.style.color = "red";
+    {
+        resultado.innerHTML = `Nota necessária para média final ser 6: <strong>${N3.toFixed(2)}</strong>`;
+        container.classList.remove("bad");
+        if (N3 <= 5)
+        {
+            resultado.style.color = "green";
+            container.classList.add("good");
+        }
+        else
+        {
+            resultado.style.color = "#0213cc"
+            container.classList.remove("good");
+        }
+    }
 }
 
 function limparCampos() {
     document.getElementById("n1").value = "";
     document.getElementById("n2").value = "";
     document.getElementById("resultado").innerHTML = "";
+    document.querySelector(".container").classList.remove("bad");
+    document.querySelector(".container").classList.remove("good");
 }
